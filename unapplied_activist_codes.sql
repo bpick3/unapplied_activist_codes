667636ee3395abef536087b5749ce6f3c50b8129
Brandon Pickett <63067820+bpick3@users.noreply.github.com>
2023-08-10 10:54:02 -0600

    Create unapplied_activist_codes.sql

---
+++ unapplied_activist_codes.sql
@@ -0,0 +1,49 @@
+WITH Common AS (
+    SELECT
+        a.activist_code_id,
+        a.activist_code_name,
+        a.activist_code_description,
+        a.is_active,
+        a.report_question,
+        a.created_by_committee_id,
+        c.committee_name,
+        a.datetime_created,
+        extract(date from max(b.datetime_modified)) AS last_date_applied,
+        count(distinct b.myv_van_id) AS qty
+    FROM 'demsutsp.vansync.activist_codes' a
+    LEFT JOIN 'demsutsp.vansync.contacts_activist_codes_myv' b ON a.activist_code_id = b.activist_code_id
+    JOIN 'demsutsp.vansync.committees_activist_codes' cac ON a.created_by_committee_id = cac.committee_id
+    JOIN 'demsutsp.vansync.committees' c ON cac.committee_id = c.committee_id
+    GROUP BY a.activist_code_id, a.activist_code_name, a.activist_code_description, a.is_active, a.report_question, a.created_by_committee_id, a.datetime_created, c.committee_name
+    HAVING qty IS NULL OR qty = 0
+    AND a.is_active = true
+)
+SELECT
+    activist_code_id,
+    'My Voters' AS category,
+    activist_code_name,
+    activist_code_description,
+    is_active,
+    created_by_committee_id,
+    committee_name,
+    datetime_created,
+    report_question,
+    last_date_applied,
+    0 AS qty  -- Set qty to 0 for consistency
+FROM Common
+
+UNION ALL
+
+SELECT
+    activist_code_id,
+    'My Campaign' AS category,
+    activist_code_name,
+    activist_code_description,
+    is_active,
+    created_by_committee_id,
+    committee_name,
+    datetime_created,
+    report_question,
+    last_date_applied,
+    0 AS qty  -- Set qty to 0 for consistency
+FROM Common;
